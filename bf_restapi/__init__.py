e1fc80944420286b2f0150148e465349ac567fa4
Diederick <diederick.de.buck@gmail.com>
2022-03-24 17:13:34 +0100

    dddd

--- bf_restapi/__init__.py
+++ bf_restapi/__init__.py
@@ -1,12 +1,12 @@
-from .bf_rest_api import put_request
-from .bf_rest_api import get_request
-from .bf_rest_api import delete_request
-from .bf_rest_api import post_request
+from bf_restapi import put_request
+from bf_restapi import get_request
+from bf_restapi import delete_request
+from bf_restapi import post_request
  
-from .bf_rest_api import HTTP_TKN_USER_AGENT
-from .bf_rest_api import HTTP_TKN_CONTENT_TYPE
-from .bf_rest_api import HTTP_TKN_ID_ENCODING
-from .bf_rest_api import HTTP_TKN_AUTHORIZATION
+from bf_restapi import HTTP_TKN_USER_AGENT
+from bf_restapi import HTTP_TKN_CONTENT_TYPE
+from bf_restapi import HTTP_TKN_ID_ENCODING
+from bf_restapi import HTTP_TKN_AUTHORIZATION
  
 __all__ = ["put_request",
            "get_request",
