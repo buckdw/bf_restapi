eb43cbd4e6578e058a1254bda9fe1479839590fa
Diederick <diederick.de.buck@gmail.com>
2022-04-05 22:36:01 +0200

    updated version, bugfix

--- bf_restapi/restapi.py
+++ bf_restapi/restapi.py
@@ -1,14 +1,76 @@
-import json
 import requests
+import enum
  
 from requests.structures import CaseInsensitiveDict
 from http import HTTPStatus
- 
+
 HTTP_TKN_USER_AGENT = 'User-Agent'
 HTTP_TKN_CONTENT_TYPE = 'Content-Type'
 HTTP_TKN_ID_ENCODING = 'Id-Encoding'
 HTTP_TKN_AUTHORIZATION = 'Authorization'
  
+
+class ContentType(enum.Enum):
+    APP_HTML = 1
+    APP_JSON = 2
+    APP_XML = 3
+    APP_FORM_URL = 4
+
+
+class EncodingType(enum.Enum):
+    PLAIN = 0
+    BASE64 = 1
+
+
+def content_type(content_type):
+    return {
+        content_type.APP_HTML: 'application/html',
+        content_type.APP_JSON: 'application/json',
+        content_type.APP_XML: 'application/xml',
+    }.get(content_type, 'application/html')
+
+
+def encoding_type(encoding_type):
+    return {
+        encoding_type.PLAIN: '0',
+        encoding_type.BASE64: '1'
+    }.get(encoding_type, '0')
+
+
+
+def header_agent(value):
+    return HTTP_TKN_USER_AGENT, value
+
+
+def header_content_type(value):
+    return HTTP_TKN_CONTENT_TYPE, content_type(value)
+
+
+def header_encoding_type(value):
+    return HTTP_TKN_ID_ENCODING, encoding_type(value)
+
+
+def header_authorization(value):
+    return HTTP_TKN_USER_AGENT, value
+
+
+#
+#   format_header:
+#       creates an https header
+#
+def format_header(**kwargs):
+    header = CaseInsensitiveDict()
+    for key, value in kwargs.items():
+        methods = {
+            'agent': header_agent,
+            'content_type': header_content_type,
+            'encoding_type': header_encoding_type,
+            'authorization': header_authorization,
+        }
+        http_header, http_value = methods[key](value)
+        header[http_header] = http_value
+    return header
+
 #
 #   post_request:
 #       creates post request with no response-buffer
@@ -35,7 +97,7 @@ def delete_request(url, headers, cacerts, valid_responses):
     except requests.exceptions.HTTPError:
         return False, result.json(), result.status_code
     except:
-        return False, msg, HTTPStatus.INTERNAL_SERVER_ERROR
+        return False, None, HTTPStatus.INTERNAL_SERVER_ERROR
     if result.status_code not in valid_responses:
         return False, None, result.status_code
     return True, None, result.status_code
